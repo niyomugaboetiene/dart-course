577a7a9359f566d9833f97c88aa34fc7ee7fadd0
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-01 17:48:55 +0200

    dart: All needed to learn dart

---
+++ variables.dart
@@ -0,0 +1,13 @@
+// ~ decraling variable using ints datatype
+// void main() {
+//   String name = "Etiene";
+//   int age = 15;
+//   print("My name is $name and may age is $age");
+// }
+
+// ? using variable var but it detects its datatype
+void main() {
+  var name = "Etiene";
+  var age = 15;
+  print("My age is $age and I'm $name");
+}
