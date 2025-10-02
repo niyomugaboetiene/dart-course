c0a1b5a6e5eb8e1b96749bdb921b71232f834df3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-02 07:47:43 +0200

    dart: Opeators

--- operators.dart
+++ operators.dart
@@ -9,6 +9,19 @@ void arithmetic() {
   print(a % b);
 }
 
+void comparison() {
+  int a = 50;
+  int b = 20;
+
+  print(a == b);
+  print(a != b);
+  print(a <= b);
+  print(a >= b);
+  print(a > b);
+  print(a < b);
+}
+
 void main() {
   arithmetic();
+  comparison();
 }
