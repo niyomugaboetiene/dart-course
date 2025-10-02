7b42fd4a5186b624dbd0a7fcbe78a2abb420d458
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-02 08:52:37 +0200

    dart: loop statements

---
+++ loops.dart
@@ -0,0 +1,12 @@
+// ~ for loop: is used when you know exact how many times to repeat
+void forloops() {
+  int a = 10;
+  for (var i = 0; i < a; i++) {
+    print(i);
+  }
+}
+
+
+void main() {
+  forloops();
+}
