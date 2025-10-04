6c41492894691a7bc500abee333d113c5281e90c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-04 14:23:05 +0200

    Dart: spread operator

---
+++ errorHandling.dart
@@ -0,0 +1,10 @@
+// * Exeception is an error happen when a program is running
+//  basic of try and catch
+void main() {
+  try {
+    int result = 10 ~/ 0;
+    print("Result: $result");
+  } catch (e) {
+    print("Errro caught: $e");
+  }
+}
