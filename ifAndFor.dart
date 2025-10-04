fe894af036e6448358d0c9950c70d6161fe998b4
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-04 11:53:12 +0200

    Dart: if and for concept

--- ifAndFor.dart
+++ ifAndFor.dart
@@ -1,6 +1,7 @@
 void main() {
   bool isLoggedIn = true;
 
+  //  ^ if you can check condition inside map, list, set
   var menu = [
     "Home",
     "About",
@@ -9,4 +10,20 @@ void main() {
   ];
 
   print(menu);
+
+  //  ? for -> you can loop inside the map, list, set
+
+  var numbers = [1, 2, 4, 5];
+
+  var squared = [for (var square in numbers) square * square];
+  print(squared);
+
+  var users = ["Etiene", "Bob", "Alice"];
+  var activeUsers = ["Etiene"];
+
+  var messages = [
+    for (var user in users) {if (activeUsers.contains(user)) "Hello $user"},
+  ];
+
+  print(messages);
 }
