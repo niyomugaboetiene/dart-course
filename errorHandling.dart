8d2f6e9096248fcc0ba5885a4abcfc05a9ab0a8a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-04 14:25:49 +0200

    Dart: Errro handling

--- errorHandling.dart
+++ errorHandling.dart
@@ -7,5 +7,15 @@ void main() {
   } catch (e) {
     print("Errro caught: $e");
   }
-}
 
+  //  using on means you can catch specicif type of error
+
+  try {
+    int res = 10 ~/ 0;
+    print("Result: $res");
+  } on IntegerDivisionByZeroException {
+    print("You can not divide number by zero");
+  } catch (e) {
+    print("Other error: $e");
+  }
+}
