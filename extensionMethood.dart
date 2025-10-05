f3c7262b195b7e4f8370302917c83643664a2500
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-05 09:16:28 +0200

    dart: typedef

--- extensionMethood.dart
+++ extensionMethood.dart
@@ -1,1 +1,13 @@
 // ~ extension methood let you add new functionality  to existing classes without modifying the origin class
+
+extension StringExtension on String {
+  bool isValidEmail() {
+    return contains("@") && contains(".");
+  }
+}
+
+void main() {
+  String email = "etiene3008@gmail.com";
+
+  print(email.isValidEmail());
+}
