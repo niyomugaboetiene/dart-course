4a85dc3d9af2c5dc19a8c45f8c924f441e870f79
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-05 08:55:09 +0200

    dart: typedef

---
+++ typedef.dart
@@ -0,0 +1,7 @@
+// ? type def is used to create alias name for a function type
+// * it make code more readable and reuse function signatures
+
+typedef intOperation = int Function(int a, int b);
+
+int add(int x, int y) => x + y;
+int multply(int x, int y) => x * y;
