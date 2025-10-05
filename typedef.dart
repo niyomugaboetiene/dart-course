71b98e223283ee46a8103deb58078c3902dbbe50
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-05 09:09:40 +0200

    dart: typedef

--- typedef.dart
+++ typedef.dart
@@ -6,3 +6,11 @@ typedef intOperation = int Function(int a, int b);
 int add(int x, int y) => x + y;
 int multply(int x, int y) => x * y;
 
+void main() {
+  intOperation operation;
+  operation = add;
+  print("Add: ${operation(3, 4)}");
+
+  operation = multply;
+  print("Multply: ${operation(3, 4)}");
+}
