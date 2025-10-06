03a80f2b6b6530add4cd8b3d936dafa74653090b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-06 10:17:38 +0200

    dart: Asychronous programming

--- asychronous.dart
+++ asychronous.dart
@@ -1,3 +1,7 @@
 // ~ Dart runs  sychronously means line by line
 // ! but if you want to use asychronous you can use future
 // ^ future represent value that will be available in future
+
+Future<String> fetchData() {
+    return 
+}
