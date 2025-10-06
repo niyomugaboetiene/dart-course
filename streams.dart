cc43001c8b47085b7822778a4a8a461b6e82cd0e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-06 12:51:00 +0200

    dart: Asychronous programming with stream

--- streams.dart
+++ streams.dart
@@ -1,2 +1,9 @@
 // * future gives you one value at time
 // ? streams give you multiple value at time
+void main() {
+  Stream<int> numbers = Stream.fromIterable([1, 2, 3, 4, 5]);
+
+  numbers.listen((number) => {
+    print("Recived number: $number");
+  });
+}
