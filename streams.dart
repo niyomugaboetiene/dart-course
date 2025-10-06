af639d1eabd673ad189d6a8fbbce46f366186bd7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-06 12:55:41 +0200

    dart: Asychronous programming with future

--- streams.dart
+++ streams.dart
@@ -1,9 +1,22 @@
 // * future gives you one value at time
 // ? streams give you multiple value at time
-void main() {
+import 'dart:async';
+
+void stream() {
   Stream<int> numbers = Stream.fromIterable([1, 2, 3, 4, 5]);
 
   numbers.listen((number) => {
-    print("Recived number: $number");
+    print("Recived number: $number")
   });
+}
+
+// ~ stream controller is used to control streams like add, remove, update and close
+
+
+void main() {
+  final controller = StreamController<String>();
+
+  controller.stream.listen(
+    (data) => print("Received")
+  )
 }
